c736057847e3a8c6fce5c51cd98c262b34ba6516
joojaein <dvpin0102@gmail.com>
2020-03-04 23:04:43 +0900

    ...

--- src/javastudy/JavaStudy.java
+++ src/javastudy/JavaStudy.java
@@ -2,8 +2,29 @@ package javastudy;
 
 public class JavaStudy {
 
+	public class Car{
+		String name;
+		int number;
+		int wheel = 4;
+		
+		void name() {
+			System.out.println("이차의 이름은" + name);
+		}
+		
+		void number() {
+			System.out.println("이차의 번호는" + number);
+		}
+		
+		void wheel() {
+			System.out.println("이차의 바퀴 수 :" + wheel);
+		}
+	}
+		
+		
 	public static void main(String[] args) {
 		
 	}
 
+	
+	
 }
