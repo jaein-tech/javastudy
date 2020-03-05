a92fb64caaaf4a6e1c66aedf231033a46edcd219
joojaein <dvpin0102@gmail.com>
2020-03-05 18:08:46 +0900

    java util package와 collection interface

--- src/javaUtill/exam/JvUtil.java
+++ src/javaUtill/exam/JvUtil.java
@@ -3,7 +3,7 @@ package javaUtill.exam;
 public class JvUtil {
 
 	public static void main(String[] args) {
-//		java의 기본제공 package중에서 많이 쓰이는 utill package, 유용한 class를 많이 가지고 있다
+//		java의 기본제공 package중에서 많이 쓰이는 util package, 유용한 class를 많이 가지고 있다
 //		ex) 자료구조와 관련된 interface와 class  / 날짜와 관련된 date, calendar
 //		
 //		deprecated - 더이상 지원하지 않는 기능이므로 사용을 자제
@@ -12,7 +12,7 @@ public class JvUtil {
 //		자료구조 = 자료를 저장할 수 있는 구조 
 		
 //		 .. Collection Interface
-//		Collection Framwork에서 가장 기본이 되는 인터페이스
+//		Collection Framework에서 가장 기본이 되는 인터페이스
 //		+ add(Object) : boolean - 컬렉션에 자료를 추가하는 메소드
 //		+ iterator() : iterator - 자료를 하나씩 꺼내기위한 이터레이터 인터페이스를 반환
 //		+ size() : int - 저장된 자료의 수를 반환하는 메소드
@@ -40,7 +40,7 @@ public class JvUtil {
 //		+ keySet() : Set - 자신이 가지고 있는 모든 key들에 대한 정보를 읽어올 수 있는 set을 반환하는 method
 
 
-//		utill package에는 위에서 설명한 interface를 구현하고 있는 class들을 많이 가지고 있다
+//		util package에는 위에서 설명한 interface를 구현하고 있는 class들을 많이 가지고 있다
 		
 //		프로그래밍을 잘 하려면 컬렉션 프레임워크에 인터페이스와 구현 클래스들을 잘 다룰줄 알아야 한다
 
